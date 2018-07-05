54c426d04f3e2ffd14a8644fbecdb7b22f2d965d
hszcz <dehydroepiandrosteron@gmail.com>
2018-07-06 00:55:47 +0200

    custom checkbox/select

---
+++ src/components/shared/select/select.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+
+class CustomSelect extends Component {
+  state = {
+    selectedOption: '',
+  };
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    // selectedOption can be null when the `x` (close) button is clicked
+    if (selectedOption) {
+      console.log(`Selected: ${selectedOption.label}`);
+    }
+  };
+  render() {
+    const { selectedOption } = this.state;
+
+    return (
+      <Select
+        clearable={false}
+        placeholder={"Wybierz rodzaj gumy"}
+        className="friction-custom-select"
+        name="form-field-name"
+        inputProps={{readOnly: true}}
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={[
+          { value: 'one', label: 'One' },
+          { value: 'two', label: 'Two' },
+        ]}
+      />
+    );
+  }
+}
+
+export default CustomSelect;
