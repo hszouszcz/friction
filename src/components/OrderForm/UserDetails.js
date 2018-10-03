ca9ab706d7e7b7639f5d080c1d009d3884a615a2
hszcz <dehydroepiandrosteron@gmail.com>
2018-10-04 01:04:48 +0200

    some select changes, user details validation in progress

--- src/components/OrderForm/UserDetails.js
+++ src/components/OrderForm/UserDetails.js
@@ -1,9 +1,33 @@
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import Select from 'react-validation/build/select';
+import validator from 'validator';
 import React, {Component} from 'react';
 import CustomSelect from '../shared/select/select';
 import {Route} from 'react-router-dom';
 import '../../scss/order-form.css';
 import ButtonsRow from '../shared/buttons/buttons-row/ButtonsRow';
 import ForwardButton from '../shared/buttons/navigation-buttons/forwardButton';
+
+const required = (value) => {
+  if (!value.toString().trim().length) {
+    // We can return string or jsx as the 'error' prop for the validated Component
+    return 'require';
+  }
+};
+
+const invoiceRequired = (value) => {
+  if (!value.toString().trim().length && this.state.invoice) {
+    // We can return string or jsx as the 'error' prop for the validated Component
+    return 'require';
+  }
+};
+
+const email = (value) => {
+  if (!validator.isEmail(value)) {
+    return `${value} is not a valid email.`
+  }
+};
 
 class UserDetails extends Component {
   constructor(props) {
@@ -34,6 +58,7 @@ class UserDetails extends Component {
       };
     }
   }
+
 
   getUserDetails = () => {
     return {
@@ -70,47 +95,54 @@ class UserDetails extends Component {
   };
 
   render() {
-    return <div className="container order-form">
+    return (<div className="container order-form">
+      <Form>
       <div className="row">
         <div className="col-md-8 offset-md-2">
           <div className="order-form-title" style={{paddingTop: '30px'}}>
             Twoje dane
-          </div>
-          <div className="order-section row no-gutters">
+          </div><div className="order-section row no-gutters">
             <div className="col-md-6">
               <label htmlFor="name">Imię</label>
-              <input
+              <Input
                 type="text"
                 id="name"
+                name="name"
                 value={this.state.name}
                 onChange={(e) => this.setState({name: e.target.value})}
+                validations={[required]}
               />
             </div>
             <div className="col-md-6">
               <label htmlFor="surname">Nazwisko</label>
-              <input
+              <Input
                 type="text"
                 id="surname"
+                name="surname"
                 value={this.state.surname}
                 onChange={(e) => this.setState({surname: e.target.value})}
+                validations={[required]}
               />
             </div>
             <div className="col-12 col-md-6">
               <label htmlFor="email">Email</label>
-              <input
+              <Input
                 type="email"
                 id="email"
                 value={this.state.email}
                 onChange={(e) => this.setState({email: e.target.value})}
+                validations={[email, required]}
               />
             </div>
             <div className="col-12 col-md-6">
               <label htmlFor="phone">Numer telefonu</label>
-              <input
+              <Input
                 type="phone"
                 id="phone"
+                name="phone"
                 value={this.state.phone}
                 onChange={(e) => this.setState({phone: e.target.value})}
+                validations={[required]}
               />
             </div>
           </div>
@@ -122,29 +154,35 @@ class UserDetails extends Component {
           <div className="order-section row no-gutters">
             <div className="col-md-6">
               <label htmlFor="street">Ulica</label>
-              <input
+              <Input
                 type="text"
                 id="street"
+                name="street"
                 value={this.state.street}
                 onChange={(e) => this.setState({street: e.target.value})}
+                validations={[required]}
               />
             </div>
             <div className="col-md-6">
               <label htmlFor="street-number">Numer ulicy/lokalu</label>
-              <input
+              <Input
                 type="text"
                 id="street-number"
+                name="street-number"
                 value={this.state.streetNumber}
                 onChange={(e) => this.setState({streetNumber: e.target.value})}
+                validations={[required]}
               />
             </div>
             <div className="col-md-6">
               <label htmlFor="city">Miasto</label>
-              <input
+              <Input
                 type="text"
                 id="city"
+                name="city"
                 value={this.state.city}
                 onChange={(e) => this.setState({city: e.target.value})}
+                validations={[required]}
               />
             </div>
             <div className="col-md-8 agreement">
@@ -163,56 +201,68 @@ class UserDetails extends Component {
             <div className="order-section row invoice">
               <div className="col-md-6">
                 <label htmlFor="name">Nazwa Firmy</label>
-                <input
+                <Input
                   type="text"
                   id="companyName"
+                  name="companyName"
                   value={this.state.invoiceDetails.name}
                   onChange={(e) => this.setState({invoiceDetails: {...this.state.invoiceDetails, name: e.target.value}})}
+                  validations={[invoiceRequired]}
                 />
               </div>
               <div className="col-md-6">
                 <label htmlFor="surname">NIP</label>
-                <input
+                <Input
                   type="text"
                   id="nip"
+                  name="nip"
                   value={this.state.invoiceDetails.nip}
                   onChange={(e) => this.setState({invoiceDetails: {...this.state.invoiceDetails, nip: e.target.value}})}
+                  validations={[invoiceRequired]}
                 />
               </div>
               <div className="col-12 col-md-6">
                 <label htmlFor="email">Ulica</label>
-                <input
+                <Input
                   type="text"
                   id="companyStreet"
+                  name="companyStreet"
                   value={this.state.invoiceDetails.street}
                   onChange={(e) => this.setState({invoiceDetails: {...this.state.invoiceDetails, street: e.target.value}})}
+                  validations={[invoiceRequired]}
                 />
               </div>
               <div className="col-12 col-md-6">
                 <label htmlFor="email">Numer Ulicy/lokalu</label>
-                <input
+                <Input
                   type="text"
-                  id="companyStreet"
+                  id="companyStreetNumber"
+                  name="companyStreetNumber"
                   value={this.state.invoiceDetails.streetNumber}
                   onChange={(e) => this.setState({invoiceDetails: {...this.state.invoiceDetails, streetNumber: e.target.value}})}
+                  validations={[invoiceRequired]}
                 />
               </div>
               <div className="col-12 col-md-3">
                 <label htmlFor="email">Kod Pocztowy</label>
-                <input
+                <Input
                   type="text"
                   id="companyPostalCode"
+                  name="postalCode"
                   value={this.state.invoiceDetails.postalCode}
                   onChange={(e) => this.setState({invoiceDetails: {...this.state.invoiceDetails, postalCode: e.target.value}})}
+                  validations={[invoiceRequired]}
                 />
               </div>
               <div className="col-12 col-md-9">
                 <label htmlFor="email">Miasto</label>
-                <input
+                <Input
                   type="text"
                   id="companyCity"
+                  name="companyCity"
                   value={this.state.invoiceDetails.city}
                   onChange={(e) => this.setState({invoiceDetails: {...this.state.invoiceDetails, city: e.target.value}})}
+                  validations={[invoiceRequired]}
                 />
               </div>
             </div>
@@ -222,16 +272,12 @@ class UserDetails extends Component {
         <div className="col-md-8 offset-md-2 ">
           <div className="order-section">
             <span className="section-title">Jak mamy odesłać Twoje buty?</span>
-            <CustomSelect
-              model={this.state.sendBack}
-              callback={(option) => this.setState({sendBack: option.value})}
-              placeholder="Wybierz"
-              options={[
-                {value: 1, label: '1'},
-                {value: 2, label: '2'},
-                {value: 3, label: '3'},
-              ]}
-            />
+            <Select name='shipping' value='' validations={[required]}>
+              <option value=''>Choose shipping</option>
+              <option value='1'>Bocian</option>
+              <option value='2'>Rakieta</option>
+              <option value='3'>New York</option>
+            </Select>
           </div>
           <div className="col-12 agreement">
             <div className="order-section">
@@ -267,8 +313,8 @@ class UserDetails extends Component {
             </ButtonsRow>
           </div>
         </div>
-      </div>
-    </div>;
+      </div></Form>
+    </div>);
   }
 };
 
